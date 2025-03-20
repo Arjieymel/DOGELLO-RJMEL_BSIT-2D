5f68db624b9b22bbc84096912bac7d18b47dff37
Arjieymel <rjmeldogello6@gmail.com>
2025-03-19 23:09:40 -0700

    battleshipv2/getting funtional

---
+++ Getting Functional/function.js
@@ -0,0 +1,43 @@
+var dogname = "chiko";
+var dogWeight = 18;
+if (dogWeight > 20) {
+    console.log(dogname + "SAYS WOOF WOOF")
+} else {
+    console.log (dogname + "says woof woof")
+}
+
+var dogname = "leopard";
+var dogWeight = 19;
+if (dogWeight > 20) {
+    console.log(dogname + "SAYS WOOF WOOF")
+} else {
+    console.log (dogname + "says woof woof")
+}
+
+var dogname = "Bantay";
+var dogWeight = 25;
+if (dogWeight > 20) {
+    console.log(dogname + "SAYS WOOF WoOOF")
+} else {
+    console.log (dogname + "says woof woof")
+}
+
+var dogname = "Musang";
+var dogWeight = 25;
+if (dogWeight > 20) {
+    console.log(dogname + "SAYS WOOF WOOF")
+} else {
+    console.log (dogname + "says woof woof")
+}
+
+
+
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + "SAYS WOOF WOOF");
+    } else{
+        console.log(name + "says woof woof");
+    }
+}
+
+bark("")
