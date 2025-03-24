4a8c1f42c00f98df32dd794ce406458924667598
Arjieymel <dogellorjmel6@gmail.com>
2025-03-24 13:57:15 +0800

    activities

---
+++ Activities/Funtion-Activity/script.js
@@ -0,0 +1,13 @@
+function whatShallWear(temp){
+    if (temp < 60) {
+        console.log("Wear a Jacket")
+    } else if (temp < 70) {
+        console.log("Wear a t-shirt");
+    } else {
+        console.log("Wear a Sweater");
+    }
+}
+
+whatShallWear(80);
+whatShallWear(60);
+whatShallWear(50);
